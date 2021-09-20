f5e1e687403a396867f5c37e2f022c860fdbef54
Raphael Moral Piazera <raphamoraleng@gmail.com>
2021-09-20 02:03:23 -0300

    Aula 4

---
+++ Mackenzie/Mackenzie Aula 4.py
@@ -0,0 +1,16 @@
+idade=int(input(""))
+
+if idade <5:
+    print('NÃO TEM IDADE PARA SER ATLETA')
+elif 5<=idade<=7:
+    print('INFANTIL A')
+elif 8<=idade<=10:
+    print('INFANTIL B')
+elif 11 <= idade <= 13:
+    print('JUVENIL A')
+elif 14 <= idade <= 17:
+    print(('JUVENIL B'))
+elif idade >=18:
+    print('SÊNIOR')
+
+
